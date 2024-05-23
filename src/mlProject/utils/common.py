c0fefec9d1c871b4b625690adb32d84461ce487c
Arivudainambi <v.a.arivudainambi19@gmail.com>
2024-05-23 20:18:47 +0530

    update Common Funtions

---
+++ src/mlProject/utils/common.py
@@ -0,0 +1,131 @@
+import os
+import yaml
+import json
+import joblib
+from pathlib import Path
+from typing import Any
+from mlProject import logger
+from box import ConfigBox
+from ensure import ensure_annotations
+from box.exceptions import BoxValueError
+
+
+
+@ensure_annotations
+def read_yaml(path: Path) -> ConfigBox:
+    """
+    Reading the YAML file
+
+    Args:
+        path (str) : file path as input
+
+    Raises:
+        e: empty file
+        ValueError: if YAML file is empty
+
+    Returns:
+        ConfigBox : ConfigBox type
+    """
+
+    try:
+        with path as p:
+            content = yaml.safe_load(p)
+            logger.info(f"yaml file: {path} loaded successfully.")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty.")
+    except Exception as e:
+        raise e
+    
+@ensure_annotations
+def create_dict(filepath: list, verbose = True):
+    """
+    Create list of dict
+
+    Args:
+        filepath (list) : list of path
+        ignore_log (bool, optional) : ignore if multiple dirs is to created Default is False
+
+    """
+
+    for path in filepath:
+        os.makedirs(path, exist_ok= True)
+        if verbose:
+            logger.info(f"created directory to : {path}")
+
+
+@ensure_annotations
+def save_json(path: Path, data:dict):
+    """
+    save the json
+
+    Args: 
+        path (path) : path to json file
+        data (dict) : data to be saved in json file
+    """
+    with open(path, 'w') as f:
+        json.dump(data, f, indent=4)
+    
+    logger.info(f"json file saved at: {path}")
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """Load the json
+
+    Args:
+        path (path) : path to json file 
+    
+    Returns:
+        ConfigBox : data as class attributes instead of dict
+    """
+    with open(path) as f:
+        content = json.load(path)
+    
+    logger.info(f"json file Successfully loaded in {path}")
+
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path : Path):
+    """save binary file
+
+    Args:
+        data (any) : data to be saved as binary
+        path (Path) : path to saved in the directory
+    
+    """
+    joblib.dump(value=data, filename=path)
+    logger.info(f"binary file saved at: {path}")
+
+
+@ensure_annotations
+def load_bin(path: Path) ->Any:
+    """Load binary data
+
+    Args:
+        path (path) : path to library file
+
+    Returns:
+        Any: Object Stored in the file
+    
+    """
+    data = joblib.load(path)
+    logger.info(f'binary file loaded from : {path}')
+    return data
+
+
+@ensure_annotations
+def get_size(path: Path):
+    """Get size of the file
+
+    Args:
+        path (Path) : path to the file
+
+    Returns:
+        str: size in KB
+    
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f'~{size_in_kb} KB'
